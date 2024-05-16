00c28abb791d71caff46360725b93e0e33aa842a
VijayKumar9389 <vijaymkumar2095@gmail.com>
2024-05-15 19:35:55 -0600

    upload server aws eb

--- dist/server.js
+++ dist/server.js
@@ -1,17 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.js
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const client_1 = require("@prisma/client");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+const prisma = new client_1.PrismaClient();
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
+app.get('/user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // Fetch user data from the database
+        const users = yield prisma.user.findMany();
+        res.json(users);
+    }
+    catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}));
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
-// src/index.js
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
+});
+
+app.get('/user', async (req: Request, res: Response) => {
+    try {
+        // Fetch user data from the database
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.listen(port, () => {
